56b37338fe03eb3f5dbd119ebbfef4799628cffa
zubairahmed66 <33100264+zubairahmed66@users.noreply.github.com>
2018-07-24 19:27:10 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,14 +4,45 @@ $(document).ready(function () {
 
     //counter
 
+
+
     $('.education_box').slick({
+        dots: false,
         infinite: true,
+        speed: 300,
         slidesToShow: 3,
         slidesToScroll: 3,
         autoplayspeed: 1000,
         autoplay: true,
-        showNextPrev: false,
-        prevArrow: '<button class="PrevArrow"></button>',
-        nextArrow: '<button class="NextArrow"></button>',
+        prevArrow: false,
+        nextArrow: false,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    dots: false
+                }
+    },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+    },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
     });
 });
